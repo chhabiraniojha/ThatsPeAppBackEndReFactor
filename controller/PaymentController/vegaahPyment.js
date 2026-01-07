f9709921e000ebe8253e0f6c69c2f17f6d6ef2d2
codechaseers <sudhanshuojha7234@gmail.com>
2026-01-07 17:46:28 +0530

    veghaa payment api testing now link url in  frontend 5

--- controller/PaymentController/vegaahPyment.js
+++ controller/PaymentController/vegaahPyment.js
@@ -1,3 +1,4 @@
+const { default: axios } = require('axios');
 const crypto = require('crypto');
 
 exports.generateSignature = async (req, res) => {
@@ -31,8 +32,7 @@ exports.generateSignature = async (req, res) => {
 exports.vegaahCallback = async (req, res) => {
   try {
     // 1️⃣ Read callback payload
-     
-  
+
     const data = req.method === 'POST' ? req.body : req.query;
     console.log('Vegaah Callback Received:', data);
     const { paymentId, responseCode, amount, signature } = data || {};
@@ -78,4 +78,50 @@ exports.vegaahCallback = async (req, res) => {
     console.error('Callback Error:', error);
     return res.status(500).send('SERVER ERROR');
   }
+};
+exports.payRequest = async (req, res) => {
+  try {
+    let trackid = Math.floor(Math.random() * 1000000 + 1);
+    const payload = { trackId: trackid, terminalId: 'TER7030747', password: 'TER26010626145585634059', amount: '01.00', currency: 'INR' };
+    const secretKey = process.env.VEGAH_SECRET_KEY;
+    // const payloadString = JSON.stringify(payload);
+
+    const dataToHash =
+      payload.trackId + '|' + payload.terminalId + '|' + payload.password + '|' + secretKey + '|' + payload.amount + '|' + payload.currency;
+
+    console.log('STRING TO HASH:--->', dataToHash);
+    if (!payload) {
+      return res.status(400).json({
+        message: 'Payload is required'
+      });
+    }
+    if (!secretKey) {
+      return res.status(500).json({
+        message: 'Secret key not configured'
+      });
+    }
+    const signature = crypto.createHash('sha256').update(dataToHash).digest('hex');
+    payRequestRequiredData = {
+      order: {
+        orderId: trackid
+      },
+      terminalId: 'TER7030747',
+      password: 'TER26010626145585634059',
+      signature: signature,
+      amount: '01.00',
+      currency: 'INR',
+      paymentType: '1',
+      customer: {
+        customerEmail: 'sudhanshuojha7234@gmail.com',
+        billingAddressCountry: 'IN'
+      }
+    };
+    const payRequestResponse = await axios.post(
+      'https://checkout.vegaah.com/vegaahpayments/v2/payments/pay-request',
+      payRequestRequiredData
+    );
+    console.log('PAY REQUEST RESPONSE:--->', payRequestResponse.data);
+    let linkurl = payRequestResponse?.data?.paymentLink?.linkUrl + payRequestResponse?.data?.transactionId;
+    return res.status(200).json({linkurl});
+  } catch (error) {}
 };
--- routes/PaymentRoutes/vegaahPyment.js
+++ routes/PaymentRoutes/vegaahPyment.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express();
-const { generateSignature ,vegaahCallback} = require('../../controller/PaymentController/vegaahPyment');
+const { generateSignature ,vegaahCallback,payRequest} = require('../../controller/PaymentController/vegaahPyment');
 
 router.post('/generate-signature', generateSignature);  
-router.post('/new-paymentlink', generateSignature);  
+router.post('/new-paymentlink', payRequest);  
 router.all('/callback', vegaahCallback);  
 
 module.exports = router;
