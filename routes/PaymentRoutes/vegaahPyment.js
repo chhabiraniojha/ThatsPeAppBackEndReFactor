ba959693c54bfe418765c1bb686a0820f748ebb5
codechaseers <sudhanshuojha7234@gmail.com>
2026-01-29 18:33:16 +0530

    vegaaha  recept controler create

--- routes/PaymentRoutes/vegaahPyment.js
+++ routes/PaymentRoutes/vegaahPyment.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express();
-const { generateSignature ,vegaahCallback,payRequest,paymentStatusCheck,orderStatusCheck} = require('../../controller/PaymentController/vegaahPyment');
+const { generateSignature ,vegaahCallback,payRequest,paymentStatusCheck,orderStatusCheck,vegaahReceipt} = require('../../controller/PaymentController/vegaahPyment');
 const Authenticate = require('../../middelWare/auth')
 
 router.post('/generate-signature', generateSignature);  
 router.post('/new-paymentlink',Authenticate, payRequest);  
 router.all('/callback', vegaahCallback);  
+router.all('/receipt', vegaahReceipt);  
 router.all('/payment-status-check', Authenticate,paymentStatusCheck);  
 router.all('/order-status-check', Authenticate,orderStatusCheck);  
 
