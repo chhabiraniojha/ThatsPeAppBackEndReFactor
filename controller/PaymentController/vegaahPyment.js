4e0aa1ac5bfbd7a08ac78255448dee0c87892b04
codechaseers <sudhanshuojha7234@gmail.com>
2026-02-04 17:39:25 +0530

    pay request  api  modification   done a ctual amount  pay 3

--- controller/PaymentController/vegaahPyment.js
+++ controller/PaymentController/vegaahPyment.js
@@ -1,5 +1,5 @@
 const { default: axios } = require('axios');
-const sequelize = require('../../config/database');
+const sequelize = require('../../util/db_connect');
 const crypto = require('crypto');
 const Order = require('../../models/OrderModel/order');
 const Payment = require('../../models/PaymentModel/payment');
