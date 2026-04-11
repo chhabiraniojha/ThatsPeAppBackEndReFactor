96e8007796901f1a40bdf6895a96e9be714cca73
chhabiraniojha <chhabiraniojha@gmail.com>
2026-04-11 15:39:35 +0530

    linkurl testing

--- controller/PaymentController/vegaahPyment.js
+++ controller/PaymentController/vegaahPyment.js
@@ -52,7 +52,7 @@ exports.payRequest = async (req, res) => {
     discountedAmount,
     purpose
   } = req.body;
-  console.log(req.body);
+  // console.log(req.body);
   const t = await sequelize.transaction();
   try {
     const clientIp = requestIp.getClientIp(req);
@@ -265,9 +265,10 @@ exports.payRequest = async (req, res) => {
       );
     }
     await t.commit();
-    console.log('PAYMENT DATA CREATED:', paymentData);
+    // console.log('PAYMENT DATA CREATED:', paymentData);
 
     let linkurl = payRequestResponse?.data?.paymentLink?.linkUrl + payRequestResponse?.data?.transactionId;
+    console.log(linkurl)
     return res.status(200).json({ linkurl, paymentId: payRequestResponse?.data?.transactionId });
   } catch (error) {
     await t.rollback();
