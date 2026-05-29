3208de491ddf03715c6885cffd42b9ca92bd7325
chhabiraniojha <chhabiraniojha@gmail.com>
2026-05-29 16:37:44 +0530

    tttttttttttttt

--- controller/PaymentController/razorPay.js
+++ controller/PaymentController/razorPay.js
@@ -358,7 +358,7 @@ exports.webhook = async (req, res) => {
         const webhookSignature =
             req.headers['x-razorpay-signature'];
 
-        const body = req.body;
+        const body = JSON.stringify(req.body);
 
         // console.log("webhookSignature..........----------" + webhookSignature)
         // console.log("body..........----------" + body)
