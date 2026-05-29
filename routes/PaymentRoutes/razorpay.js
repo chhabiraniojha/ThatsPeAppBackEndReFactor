39d3117bfbfe858b909a8f7e5cec89ae1e774fb7
chhabiraniojha <chhabiraniojha@gmail.com>
2026-05-29 12:27:58 +0530

    webhook work under progress

--- routes/PaymentRoutes/razorpay.js
+++ routes/PaymentRoutes/razorpay.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express();
-const  razorpayPaymentController= require('../../controller/PaymentController/razorPay');
+const razorpayPaymentController = require('../../controller/PaymentController/razorPay');
 const Authenticate = require('../../middelWare/auth')
 
 // router.post('/generate-signature', razorpayPaymentController.generateSignature);  
-router.post('/create-order',Authenticate, razorpayPaymentController.payRequest);  
-router.post('/verify-payment',Authenticate, razorpayPaymentController.verifyPayment);  
-router.all('/webhook', razorpayPaymentController.webhook);  
+router.post('/create-order', Authenticate, razorpayPaymentController.payRequest);
+router.post('/verify-payment', Authenticate, razorpayPaymentController.verifyPayment);
+router.all('/webhook', express.raw({type: 'application/json'}), razorpayPaymentController.webhook);
 // router.all('/payment-status-check', Authenticate,razorpayPaymentController.paymentStatusCheck);  
 // router.all('/order-status-check', Authenticate,razorpayPaymentController.orderStatusCheck);  
 
