d12920977ed2a611b8159429478c1185fd8307d3
chhabiraniojha <chhabiraniojha@gmail.com>
2026-08-22 13:07:11 +0530

    testing under progress

--- controller/PaymentController/zaakPay.js
+++ controller/PaymentController/zaakPay.js
@@ -657,7 +657,7 @@ exports.webhook = async (req, res) => {
         // 5. Verify Zaakpay webhook checksum
         // --------------------------------------------------
 
-        const isValidChecksum = validateZaakpayWebhookChecksum(
+        const isValidChecksum = zaakpayChecksum.validateZaakpayWebhookChecksum(
             txnData,
             checksum
         );
